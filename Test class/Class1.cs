bce1df62d472899683f51ca1482303770da0da08
Lenovo <Lenovo@FENIL>
2024-06-08 12:50:19 -0400

    Add project files.

---
+++ Test class/Class1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NUnitDemo;
+using System;
+
+[TestFixture]
+public class TriangleTests
+{
+    private object Assert;
+
+    [Test]
+    public void TestValidTriangle()
+    {
+        Assert.AreEqual("The triangle is valid.", Triangle.ValidTriangle(60, 60, 60));
+        Assert.AreEqual("The triangle is valid.", Triangle.ValidTriangle(90, 45, 45));
+    }
+
+    [Test]
+    public void TestInvalidTriangle()
+    {
+        Assert.AreEqual("The triangle is NOT valid.", Triangle.ValidTriangle(30, 60, 90));
+        object value = Assert.AreEqual("The triangle is NOT valid.", Triangle.ValidTriangle(0, 0, 0));
+    }
+}
+
+internal class Triangle
+{
+    internal static object ValidTriangle(int v1, int v2, int v3)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class TestAttribute : Attribute
+{
+}
